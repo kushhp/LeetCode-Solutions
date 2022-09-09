9de66d71c9ef26c55e45f1f08fae526d5d4d058a
Kush Patel <60983239+kushhp@users.noreply.github.com>
2022-09-09 13:41:52 -0400

    Time: 63 ms (10.93%), Space: 13.8 MB (62.03%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def breakPalindrome(self, palindrome):
+        s = palindrome
+        palindrome = [ch for ch in s]
+        if len(palindrome) == 1:
+            return ""
+        
+        for i in range(len(palindrome)//2):
+            if palindrome[i] != 'a':
+                palindrome[i] = 'a'
+                return "".join(palindrome)
+        palindrome[-1] = 'b'
+        return "".join(palindrome)
