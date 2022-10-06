e77ed77f5a9bc72797e3d27807deda04fcf4f7d2
Kush Patel <60983239+kushhp@users.noreply.github.com>
2022-10-05 21:20:05 -0400

    Time: 631 ms (5.09%), Space: 14.8 MB (47.70%) - LeetHub

---
+++ 1656-design-an-ordered-stream/1656-design-an-ordered-stream.py
@@ -0,0 +1,15 @@
+class OrderedStream:
+
+    def __init__(self, n: int):
+        self.stream = [''] * (n + 1)
+        self.i = 0
+
+    def insert(self, idKey: int, value: str) -> List[str]:
+        self.stream[idKey - 1] = value
+        res = []
+
+        while self.stream[self.i]:
+            res.append(self.stream[self.i])
+            self.i += 1
+        
+        return res
